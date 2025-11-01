0322086f7a2cd488b1ed4b826d2c6a018d1c3c62
Am0rfu5 <1178902+Am0rfu5@users.noreply.github.com>
2025-11-01 19:18:09 +0000

    fix: linting warnings in Diamond.ts

--- src/core/Diamond.ts
+++ src/core/Diamond.ts
@@ -1,194 +1,208 @@
-import { Signer } from "ethers";
-import { join } from "path";
-import { DeploymentRepository } from "../repositories/DeploymentRepository";
+import { Signer } from 'ethers';
+import { join } from 'path';
+import { DeploymentRepository } from '../repositories/DeploymentRepository';
 import {
   DeployConfig,
   DeployedDiamondData,
   DeployedFacets,
-  FacetsConfig
-} from "../schemas";
+  FacetsConfig,
+} from '../schemas';
 import {
   DiamondConfig,
   FunctionSelectorRegistryEntry,
   NewDeployedFacet,
   NewDeployedFacets,
   RegistryFacetCutAction,
-  SupportedProvider
-} from "../types";
-import { CallbackManager } from "./CallbackManager";
+  SupportedProvider,
+} from '../types';
+import { CallbackManager } from './CallbackManager';
 
 export class Diamond {
-  private static instances: Map<string, Diamond> = new Map();
+	private static instances: Map<string, Diamond> = new Map();
 
-  public diamondName: string;
-  public networkName: string;
-  public chainId: number | bigint;
-  public deploymentsPath: string;
-  public contractsPath: string;
-  public diamondAbiPath: string;
-  public diamondAbiFileName: string;
-  public deploymentId: string;
-  public facetSelectors: string[] = [];
-  public callbackManager: CallbackManager;
-  private deployedDiamondData: DeployedDiamondData;
-  private config: DiamondConfig;
-  private facetsConfig: FacetsConfig;
-  private repository: DeploymentRepository;
-  public signer: Signer | undefined;
-  public provider: SupportedProvider | undefined;
-  public deployConfig: DeployConfig;
-  public newDeployment: boolean = true;
-  public initAddress: string | undefined;
+	public diamondName: string;
+	public networkName: string;
+	public chainId: number | bigint;
+	public deploymentsPath: string;
+	public contractsPath: string;
+	public diamondAbiPath: string;
+	public diamondAbiFileName: string;
+	public deploymentId: string;
+	public facetSelectors: string[] = [];
+	public callbackManager: CallbackManager;
+	private deployedDiamondData: DeployedDiamondData;
+	private config: DiamondConfig;
+	private facetsConfig: FacetsConfig;
+	private repository: DeploymentRepository;
+	public signer: Signer | undefined;
+	public provider: SupportedProvider | undefined;
+	public deployConfig: DeployConfig;
+	public newDeployment: boolean = true;
+	public initAddress: string | undefined;
 
-  constructor(config: DiamondConfig, repository: DeploymentRepository) {
-    this.config = config;
-    this.diamondName = config.diamondName;
-    this.networkName = config.networkName ?? "hardhat";
-    this.chainId = config.chainId ?? 31337;
-    this.deploymentsPath = config.deploymentsPath ?? "diamonds";
-    this.contractsPath = config.contractsPath ?? "contracts";
-    this.diamondAbiFileName = config.diamondAbiFileName ?? config.diamondName;
-    
-    // Set diamond ABI path - default to diamond-abi subdirectory of configFilePath directory
-    if (config.diamondAbiPath) {
-      this.diamondAbiPath = config.diamondAbiPath;
-    } else {
-      const configDir = config.configFilePath 
-        ? config.configFilePath.replace(/\/[^\/]*$/, '') // Remove filename from path
-        : join(this.deploymentsPath, config.diamondName);
-      this.diamondAbiPath = join(configDir, 'diamond-abi');
-    }
-    
-    this.repository = repository;
-    this.deploymentId = repository.getDeploymentId();
+	constructor(config: DiamondConfig, repository: DeploymentRepository) {
+		this.config = config;
+		this.diamondName = config.diamondName;
+		this.networkName = config.networkName ?? 'hardhat';
+		this.chainId = config.chainId ?? 31337;
+		this.deploymentsPath = config.deploymentsPath ?? 'diamonds';
+		this.contractsPath = config.contractsPath ?? 'contracts';
+		this.diamondAbiFileName = config.diamondAbiFileName ?? config.diamondName;
 
-    // Load existing deployment info
-    this.deployedDiamondData = this.repository.loadDeployedDiamondData();
-    this.deployConfig = this.repository.loadDeployConfig();
+		// Set diamond ABI path - default to diamond-abi subdirectory of configFilePath directory
+		if (config.diamondAbiPath) {
+			this.diamondAbiPath = config.diamondAbiPath;
+		} else {
+			const configDir = config.configFilePath
+				? config.configFilePath.replace(/\/[^\/]*$/, '') // Remove filename from path
+				: join(this.deploymentsPath, config.diamondName);
+			this.diamondAbiPath = join(configDir, 'diamond-abi');
+		}
 
-    this.facetsConfig = this.deployConfig.facets;
+		this.repository = repository;
+		this.deploymentId = repository.getDeploymentId();
 
-    this.callbackManager = CallbackManager.getInstance(
-      this.diamondName, this.deploymentsPath);
+		// Load existing deployment info
+		this.deployedDiamondData = this.repository.loadDeployedDiamondData();
+		this.deployConfig = this.repository.loadDeployConfig();
 
-    this._initializeFunctionSelectorRegistry(this);
-  }
+		this.facetsConfig = this.deployConfig.facets;
 
-  public functionSelectorRegistry = new Map<string, FunctionSelectorRegistryEntry>();
+		this.callbackManager = CallbackManager.getInstance(
+			this.diamondName,
+			this.deploymentsPath,
+		);
 
-  private _initializeFunctionSelectorRegistry(
-    diamond: Diamond
-  ) {
-    const diamondConfig: DiamondConfig = diamond.getDiamondConfig();
-    const deployedDiamondData: DeployedDiamondData = diamond.getDeployedDiamondData();
-    const deployedFacets: DeployedFacets = deployedDiamondData.DeployedFacets ?? {};
+		this._initializeFunctionSelectorRegistry(this);
+	}
 
-    for (const [facetName, { address: contractAddress, funcSelectors: selectors }] of Object.entries(deployedFacets)) {
-      console.log(facetName);
-      for (const selector of selectors!) {
-        this.functionSelectorRegistry.set(selector, {
-          facetName,
-          priority: this.facetsConfig[facetName]?.priority! ?? 1000,
-          address: contractAddress!,
-          action: RegistryFacetCutAction.Deployed,
-        });
-      }
-    }
-  }
+	public functionSelectorRegistry = new Map<string, FunctionSelectorRegistryEntry>();
 
-  public registerFunctionSelectors(selectors: Record<string, Omit<FunctionSelectorRegistryEntry, "selector">>): void {
-    Object.entries(selectors).forEach(([selector, entry]) => {
-      this.functionSelectorRegistry.set(selector, entry);
-    });
-  }
+	private _initializeFunctionSelectorRegistry(diamond: Diamond): void {
+		const diamondConfig: DiamondConfig = diamond.getDiamondConfig();
+		const deployedDiamondData: DeployedDiamondData = diamond.getDeployedDiamondData();
+		const deployedFacets: DeployedFacets = deployedDiamondData.DeployedFacets ?? {};
 
-  public updateFunctionSelectorRegistry(selector: string, entry: FunctionSelectorRegistryEntry): void {
-    this.functionSelectorRegistry.set(selector, entry);
-  }
+		for (const [
+			facetName,
+			{ address: contractAddress, funcSelectors: selectors },
+		] of Object.entries(deployedFacets)) {
+			console.log(facetName);
+			if (!selectors || !contractAddress) {
+				continue;
+			}
+			for (const selector of selectors) {
+				const facetConfig = this.facetsConfig[facetName as keyof FacetsConfig];
+				const priority = facetConfig?.priority ?? 1000;
+				this.functionSelectorRegistry.set(selector, {
+					facetName,
+					priority,
+					address: contractAddress,
+					action: RegistryFacetCutAction.Deployed,
+				});
+			}
+		}
+	}
 
-  public isFunctionSelectorRegistered(selector: string): boolean {
-    return this.functionSelectorRegistry.has(selector);
-  }
+	public registerFunctionSelectors(
+		selectors: Record<string, Omit<FunctionSelectorRegistryEntry, 'selector'>>,
+	): void {
+		Object.entries(selectors).forEach(([selector, entry]) => {
+			this.functionSelectorRegistry.set(selector, entry);
+		});
+	}
 
-  public newDeployedFacets: NewDeployedFacets = {};
+	public updateFunctionSelectorRegistry(
+		selector: string,
+		entry: FunctionSelectorRegistryEntry,
+	): void {
+		this.functionSelectorRegistry.set(selector, entry);
+	}
 
-  public getNewDeployedFacets(): NewDeployedFacets {
-    return this.newDeployedFacets ?? {};
-  }
+	public isFunctionSelectorRegistered(selector: string): boolean {
+		return this.functionSelectorRegistry.has(selector);
+	}
 
-  public updateNewDeployedFacets(facetName: string, facet: NewDeployedFacet): void {
-    this.newDeployedFacets[facetName] = facet;
-  }
+	public newDeployedFacets: NewDeployedFacets = {};
 
-  getDeployedDiamondData(): DeployedDiamondData {
-    return this.deployedDiamondData;
-  }
+	public getNewDeployedFacets(): NewDeployedFacets {
+		return this.newDeployedFacets ?? {};
+	}
 
-  setDeployedDiamondData(data: DeployedDiamondData) {
-    this.deployedDiamondData = data;
-  }
+	public updateNewDeployedFacets(facetName: string, facet: NewDeployedFacet): void {
+		this.newDeployedFacets = this.newDeployedFacets ?? {};
+		this.newDeployedFacets[facetName as keyof NewDeployedFacets] = facet;
+	}
 
-  updateDeployedDiamondData(data: DeployedDiamondData): void {
-    this.deployedDiamondData = data;
-    this.repository.saveDeployedDiamondData(data);
-  }
+	getDeployedDiamondData(): DeployedDiamondData {
+		return this.deployedDiamondData;
+	}
 
-  public getDiamondConfig(): DiamondConfig {
-    return this.config;
-  }
+	setDeployedDiamondData(data: DeployedDiamondData): void {
+		this.deployedDiamondData = data;
+	}
 
-  public getDeployConfig(): DeployConfig {
-    return this.deployConfig;
-  }
+	updateDeployedDiamondData(data: DeployedDiamondData): void {
+		this.deployedDiamondData = data;
+		this.repository.saveDeployedDiamondData(data);
+	}
 
-  public getFacetsConfig(): FacetsConfig {
-    return this.facetsConfig;
-  }
+	public getDiamondConfig(): DiamondConfig {
+		return this.config;
+	}
 
-  public setProvider(provider: SupportedProvider): void {
-    this.provider = provider;
-  }
+	public getDeployConfig(): DeployConfig {
+		return this.deployConfig;
+	}
 
-  public getProvider(): SupportedProvider | undefined {
-    return this.provider;
-  }
+	public getFacetsConfig(): FacetsConfig {
+		return this.facetsConfig;
+	}
 
-  public setSigner(signer: Signer): void {
-    this.signer = signer;
-  }
+	public setProvider(provider: SupportedProvider): void {
+		this.provider = provider;
+	}
 
-  public getSigner(): Signer | undefined {
-    return this.signer;
-  }
+	public getProvider(): SupportedProvider | undefined {
+		return this.provider;
+	}
 
-  public isUpgradeDeployment(): boolean {
-    return !!this.deployedDiamondData.DiamondAddress;
-  }
+	public setSigner(signer: Signer): void {
+		this.signer = signer;
+	}
 
-  public initializerRegistry: Map<string, string> = new Map();
+	public getSigner(): Signer | undefined {
+		return this.signer;
+	}
 
-  public registerInitializers(facetName: string, initFunction: string): void {
-    this.initializerRegistry.set(facetName, initFunction);
-  }
+	public isUpgradeDeployment(): boolean {
+		return !!this.deployedDiamondData.DiamondAddress;
+	}
 
-  public setInitAddress(initAddress: string): void {
-    this.initAddress = initAddress;
-  }
-  public getInitAddress(): string | undefined {
-    return this.initAddress;
-  }
+	public initializerRegistry: Map<string, string> = new Map();
 
-  public getDiamondAbiPath(): string {
-    return this.diamondAbiPath;
-  }
+	public registerInitializers(facetName: string, initFunction: string): void {
+		this.initializerRegistry.set(facetName, initFunction);
+	}
 
-  public getDiamondAbiFileName(): string {
-    return this.diamondAbiFileName;
-  }
+	public setInitAddress(initAddress: string): void {
+		this.initAddress = initAddress;
+	}
+	public getInitAddress(): string | undefined {
+		return this.initAddress;
+	}
 
-  public getDiamondAbiFilePath(): string {
-    return join(this.diamondAbiPath, `${this.diamondAbiFileName}.json`);
-  }
+	public getDiamondAbiPath(): string {
+		return this.diamondAbiPath;
+	}
+
+	public getDiamondAbiFileName(): string {
+		return this.diamondAbiFileName;
+	}
+
+	public getDiamondAbiFilePath(): string {
+		return join(this.diamondAbiPath, `${this.diamondAbiFileName}.json`);
+	}
 }
 
 export default Diamond;
